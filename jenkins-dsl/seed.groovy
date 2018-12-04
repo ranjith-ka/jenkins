5840bbcc1b84541f845bd377e6e6b95f759ab13f
Ranjith KA <ranjith.ka@dynacommerce.com>
2018-12-04 23:15:49 +0530

    Add View

--- jenkins-dsl/seed.groovy
+++ jenkins-dsl/seed.groovy
@@ -70,3 +70,23 @@ for (bitbucket_repo in bitbucket_repos)
     }
   }
 }
+
+listView('Infra Jobs') {
+description('All new jobs for testlist')
+filterBuildQueue()
+filterExecutors()
+jobs {
+    name('infra') 
+    name('cake') 
+
+}
+    columns {
+    status()
+    weather()
+    name()
+    lastSuccess()
+    lastFailure()
+    lastDuration()
+    buildButton()
+}
+}
